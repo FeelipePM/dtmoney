3d18c68df0d1db5d7f10467d8b59ee09ab482e8e
Felipe <Feelipe@email.com>
2021-06-16 16:35:13 -0300

    Component: Summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from './components/Header';
+import { Dashboard } from './components/Dashboard';
 
 import { GlobalStyle } from "./styles/global";
 
-export function App() {
+export const App = () => {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
